1c3baf21db7efbdfdeab4b159c549b8cb52c5ead
Intile-dev <gaelintile@gmail.com>
2026-06-29 12:12:15 -0300

    variables-python

---
+++ 2-user-input.py
@@ -0,0 +1,18 @@
+#User input
+
+favourite_food = input("what is your favorite food?")
+
+print(f"your favourite food is {favourite_food}")
+
+
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
+add = number1 + number2
+sub = number1 - number2
+mult = number1 * number2
+div = number1 / number2
+
+print(f"{number1} + {number2} = {add}")
+print(f"{number1} - {number2} = {sub}")
+print(f"{number1} * {number2} = {mult}")
+print(f"{number1} / {number2} = {div}")
