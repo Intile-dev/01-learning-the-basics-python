c03e42709d7c5603879186cbd4950e0a875f44cf
Intile-dev <gaelintile@gmail.com>
2026-06-28 21:55:49 -0300

    variables-python

--- variables.py
+++ variables.py
@@ -1,5 +1,12 @@
 #Variables
 
+age = 16
+gpa = 3.3
 favourite_food = "pizza"
+i_have_pizza = False
 
+print(f"I am {age} years old")
+print(f"My GPA is {gpa}")
 print(f"mi favourite food is {favourite_food}")
+print(f"I have pizza is:{i_have_pizza}")
+
