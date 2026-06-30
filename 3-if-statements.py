f333e5ad3382d6bbe808d506f976c8dd41bf2565
Intile-dev <gaelintile@gmail.com>
2026-06-30 09:58:16 -0300

    comments for 3-if statements

--- 3-if-statements.py
+++ 3-if-statements.py
@@ -2,30 +2,29 @@
 
 #rock, paper, scissors game
 
-import random
+import random #this takes the library to be able to use random
 
-options = ("rock", "paper", "scissors")
+options = ("rock", "paper", "scissors") #the options available
 
-computer_choice = random.choice(options)
+computer_choice = random.choice(options) #the computer chooses a random options between thew options available
 
 print("Let's play rock paper scissors")
 
-user_options = int(input("enter 1 for rock, 2 for paper, or 3 for scissors: "))
+user_options = int(input("enter 1 for rock, 2 for paper, or 3 for scissors: ")) #this asks you to choose between 1, 2 or 3
 user_choice = ""
 
-if user_options == 1:
+if user_options == 1:        #this takes the number you chose and translates it to rock, papers or scissors to store it in user_choice
     print("You chose rock")
     user_choice = "rock"
-if user_options == 2:
+elif user_options == 2:
     print("You chose paper")
     user_choice = "paper"
-if user_options == 3:
+elif user_options == 3:
     print("You chose scissors")
     user_choice = "scissors"
 
 
-
-if computer_choice == "rock" and user_choice == "rock":
+if computer_choice == "rock" and user_choice == "rock": #these operations are the possible combinations for the computer and user of rock, paper and scissors
     print("computer: rock")
     print("you: rock")
     print("It's a draw")
@@ -63,7 +62,6 @@ if computer_choice == "scissors" and user_choice == "scissors":
     print("computer: scissors")
     print("you: scissors")
     print("It's a draw")
-else:
-    print("invalid option")
+
 
 
