92a0439a1ef201e6698b6a0b6ffc949cf6ed19fd
Intile-dev <gaelintile@gmail.com>
2026-07-04 22:45:45 -0300

    added monthly contributions to the compund interest calculator

--- 4-compound-interest-calculator.py
+++ 4-compound-interest-calculator.py
@@ -3,11 +3,13 @@
 #compound calculator
 
 print("Hello, this is a compound interest calculator")
-initial_amount = int(input("Enter your initial amount of money: "))
-interest = float(input("Enter the interest you have: "))
-time_in_years = int(input("Enter the amount of time in years the money has to compound: "))
+initial_amount = int(input("Enter your initial amount of money: ")) #the initial amound of money you have
+interest = float(input("Enter the interest you have: ")) #the interest you have
+time_in_years = int(input("Enter the amount of time in years the money has to compound: ")) #the amount of years the money has to compound
+monthly_contribution = int(input("Enter the amount of monthly contribution: "))
 
-result = initial_amount * (1 + (interest / 100)) ** time_in_years
+interest_monthly = (interest / 100) / 12
+time_in_months = time_in_years * 12
+result = initial_amount * (1 + interest_monthly) ** time_in_months + monthly_contribution * ((1+interest_monthly) ** time_in_months -1) / interest_monthly #the compound interest formula with monthly contributions (I had to look it up on internet)
 
-
-print(f"With the initial amount of ${initial_amount} In {time_in_years} years with {interest}% you will have ${round(result)}")
+print(f"With the initial amount of ${initial_amount} In {time_in_years} years with {interest}% you will have ${round(result)}") #result
